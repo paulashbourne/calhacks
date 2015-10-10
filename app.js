acbe34f4b142b7560d17bd85a75c07e94ebf5e59
Paul Ashbourne <paul.ashbourne@hotmail.com>
2015-10-10 13:01:55 -0700

    Add mongo

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://paulashbourne.com/calhacks');
 
 app.get('/', function(req, res) {
   res.json({message: "Hello Paul"})
