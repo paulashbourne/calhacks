fbd5b9def400103a9fcc190b6aa5b83967e4ab61
Paul Ashbourne <paul.ashbourne@hotmail.com>
2015-10-10 15:04:21 -0700

    Routes

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,17 @@
+var express = require('express');
+
+var router = express.Router();
+
+router.use(function(req, res, next) {
+  var datestr = Date.now();
+  console.log(req.method + ' ' + req.path + '['+datestr+']');
+  next();
+});
+
+router.get('/', function(req, res) {
+  res.json({message: 'Hello World'});   
+});
+
 module.exports = function (app) {
-  app.get("/", function (req, res) {
-    res.json({message: "Hello World"})
-  });
+  app.use('/api', router);
 };
