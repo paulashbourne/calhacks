6901e94c4ae3fdbb3760400ca0e6a6899afde8b7
Matthew Kuzyk <matthew.kuzyk@gmail.com>
2015-10-10 13:04:40 -0700

    Added User model

---
+++ app/models/user.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var UserSchema = new Schema({
+    name: {
+        first: String,
+        last: String
+    },
+    address: {
+        street: String,
+        city: String,
+        state: String,
+        zip: String
+    },
+    phone: String
+})
+
+module.exports = mongoose.model('User', UserSchema)
