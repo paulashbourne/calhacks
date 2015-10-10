d71afd69ac5fe2974bb64d82b043d6f81883783f
Paul Ashbourne <paul.ashbourne@hotmail.com>
2015-10-10 14:17:53 -0700

    External routing

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ var app = express();
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://paulashbourne.com/calhacks');
 
-app.get('/', function(req, res) {
-  res.json({message: "Hello Paul"})
-})
+require('./app/routes.js')(app);
 
 app.listen(8002, function () {
   console.log('Hacks is running, yo');
