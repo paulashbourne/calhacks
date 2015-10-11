a0a115433a447a00846734d28516d1f7f949698b
Paul Ashbourne <paul.ashbourne@hotmail.com>
2015-10-10 19:39:22 -0700

    Add web socket

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
+// Libraries
 var express   = require('express');
 var mongoose  = require('mongoose');
+var http      = require('http');
+var io        = require('socket.io')(http);
 
+// Local imports
 var constants = require('./app/constants');
 
+// Initialize app
 var app = express();
 app.constants = constants;
+
+// Conntext to mongo
 mongoose.connect('mongodb://paulashbourne.com/calhacks');
 
+// Set up routes
 require('./app/routes/router')(app);
 
-var PORT = 8002;
-app.listen(PORT, function () {
-  console.log('Server is running on port ' + PORT);
+// Set up http server
+var server = http.Server(app);
+var port = 8002;
+server.listen(port, function () {
+  console.log('Server is running on port ' + port);
+});
+
+// Socket
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
 });
