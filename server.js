a94a6f12fe4ff284337e58f8dec13acab7a77324
Matthew Kuzyk <matthew.kuzyk@gmail.com>
2015-10-11 07:55:10 -0700

    Sockets work

--- server.js
+++ server.js
@@ -2,29 +2,34 @@
 var express   = require('express');
 var mongoose  = require('mongoose');
 var http      = require('http');
-var io        = require('socket.io')(http);
 
 // Local imports
 var constants = require('./app/constants');
 
 // Initialize app
 var app = express();
+var server = http.Server(app);
+var io        = require('socket.io')(server);
+
 app.constants = constants;
 
 // Conntext to mongo
 mongoose.connect('mongodb://paulashbourne.com/calhacks');
+app.use(express.static('app/public'));
 
-// Set up routes
 require('./app/routes/router')(app);
 
+
 // Set up http server
-var server = http.Server(app);
 var port = 8002;
+// Socket
+
+
 server.listen(port, function () {
   console.log('Server is running on port ' + port);
 });
 
-// Socket
+// Set up routes
 var User = require('./app/model/user')
 io.on('connection', function(socket){
   console.log('a user connected');
